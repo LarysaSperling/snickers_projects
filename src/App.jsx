7404e62988c9fa7266d6b98cffed4bdcef2c1d6d
Larysa <lorasperling@gmail.com>
2026-03-25 11:36:14 +0100

    feat: add App.jsx component

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,23 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Header from "./components/header";
+import Footer from "./components/footer";
+import Main from "./components/main";
+import Cart from "./components/cart";
+import Contact from "./components/contact";
 
+function App() {
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/contacts" element={<Contact />} />
+      </Routes>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
